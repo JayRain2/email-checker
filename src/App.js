7cbd81508bd47e0d3997b7f9ef0d21ca93970e20
Jason <crazyjaygamer578@gmail.com>
2023-02-07 19:10:37 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [ state, setState ] = React.useState(false);
+
+  function onChange(event) {
+	const emailText = event.target.value;
+	return emailText.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g) ? setState(true) : setState(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="mainApp">
+		<h1 style={{margin: 0}}>Check email</h1>
+		<div className="email">
+			<input onChange={onChange} placeholder="test-email@gmail.com" />
+			<h2 style={{
+				marginLeft: 5, 
+				color: state ? "green" : "red",}}>{state ? "Valid Email" : "Invalid email!"}</h2>
+		</div>
+
+	</div>
   );
 }
 
